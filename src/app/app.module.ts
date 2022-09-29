882760eac6ba8c844eb02e72ef34f9faeb2421a6
Leonel Burgos <leobursaenz23@gmail.com>
2022-09-29 03:36:45 -0300

    primera entrega

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './component/navbar/navbar.component';
+import { ToolbarComponent } from './component/toolbar/toolbar.component';
+import { StudentComponent } from './component/student/student.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    ToolbarComponent,
+    StudentComponent
   ],
   imports: [
     BrowserModule,
@@ -16,3 +24,4 @@ import { AppComponent } from './app.component';
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+  
